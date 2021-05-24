d081244206174606082d6781d7c67384ea46e2e4
fsdexter <fsdexter@gmail.com>
2021-05-24 18:50:11 +0000

    domain gennerator, app.js added, index.html added,

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#domains").innerHTML = newDomains();
+
+  function newDomains() {
+    let pro = ["the", "our"]; //i
+    let adj = ["great", "big"]; //j
+    let non = ["jogger", "racoon"]; //k
+    let dom = [".com", ".net", ".es"]; //l
+
+    let asdf = "";
+
+    for (var i = 0; i < pro.length; i++) {
+      for (var j = 0; j < adj.length; j++) {
+        for (var k = 0; k < non.length; k++) {
+          for (var l = 0; l < dom.length; l++) {
+            asdf =
+              asdf + ("<li>" + pro[i] + adj[j] + non[k] + dom[l] + "</li>");
+          }
+        }
+      }
+    }
+    return asdf;
+  }
 };
